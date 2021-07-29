c2e986f1c155517ddf1921a84ac0761f541902f3
1chang1 <aarh4n@gmail.com>
2021-07-29 09:59:33 +0900

    Add winston daily logger

--- src/loaders/logger.ts
+++ src/loaders/logger.ts
@@ -1,19 +1,42 @@
 import winston from "winston";
+import winstonDaily from "winston-daily-rotate-file";
+
 import config from "../config";
 
-const transports = [];
-if (process.env.NODE_ENV !== "development") {
-  transports.push(new winston.transports.Console());
-} else {
-  transports.push(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.cli(),
-        winston.format.splat()
-      ),
-    })
-  );
-}
+const logDir = "./logs"; // logs 디렉토리 하위에 로그 파일 저장
+
+const transports = [
+  // info 레벨 로그를 저장할 파일 설정
+  new winstonDaily({
+    level: "info",
+    datePattern: "YYYY-MM-DD",
+    dirname: logDir,
+    filename: `%DATE%.log`,
+    maxFiles: 30, // 30일치 로그 파일 저장
+    zippedArchive: true,
+  }),
+  // error 레벨 로그를 저장할 파일 설정
+  new winstonDaily({
+    level: "error",
+    datePattern: "YYYY-MM-DD",
+    dirname: logDir + "/error", // error.log 파일은 /logs/error 하위에 저장
+    filename: `%DATE%.error.log`,
+    maxFiles: 30,
+    zippedArchive: true,
+  }),
+];
+// if (process.env.NODE_ENV !== "development") {
+//   transports.push(new winston.transports.Console());
+// } else {
+//   transports.push(
+//     new winston.transports.Console({
+//       format: winston.format.combine(
+//         winston.format.cli(),
+//         winston.format.splat()
+//       ),
+//     })
+//   );
+// }
 
 const LoggerInstance = winston.createLogger({
   level: config.logs.level,
