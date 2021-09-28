22e250b38848a9c0d48935c875cfb353603c6269
henry <cherryme@kakao.com>
2021-09-28 10:34:35 +0900

    Fix: "GET /api/fields", response everything

--- src/services/field.ts
+++ src/services/field.ts
@@ -16,6 +16,41 @@ export default class fieldService {
     const db = Container.get<mysql2.Connection>("db");
     const queryGetFieldsList = `
       SELECT * FROM Field`;
+    //----------------------------
+    // const query = `SELECT * FROM BigField`;
+    // const [dbResult] = await db.query(query);
+    // const pResult = JSON.parse(JSON.stringify(dbResult));
+
+    // const query2 = `SELECT * FROM Field`;
+    // const [dbResult2] = await db.query(query2);
+    // const pResult2 = JSON.parse(JSON.stringify(dbResult2));
+
+    // let result = { bigField: [] } as any;
+
+    // let idx = 1;
+    // for (let i = 0; i < 14; i++) {
+    //   let json = {} as any;
+    //   json.id = pResult[i].big_field_id;
+    //   json.name = pResult[i].big_field;
+    //   json.smallGroup = [];
+
+    //   for (let j = 0; j < pResult2.length; j++) {
+    //     if (pResult2[j].big_field_id == idx) {
+    //       let sjson = {} as any;
+    //       sjson.id = pResult2[j].field_id;
+    //       sjson.name = pResult2[j].field;
+    //       json.smallGroup.push(sjson);
+    //     } else if (pResult2[j].big_field_id < idx) {
+    //       continue;
+    //     } else {
+    //       idx++;
+    //       break;
+    //     }
+    //   }
+
+    //   result.bigField.push(json);
+    // }
+    //----------------------------
     const [queryGetFieldsListResult] = await db.query(queryGetFieldsList);
     return queryGetFieldsListResult;
   }
--- src/services/test.ts
+++ src/services/test.ts
@@ -1,6 +1,8 @@
 import { Container, Service, Inject } from "typedi";
 import mysql2 from "mysql2/promise";
 import { Logger } from "winston";
+import * as fs from "fs";
+import * as path from "path";
 
 @Service()
 export default class TestService {
@@ -9,46 +11,51 @@ export default class TestService {
   public async dbTest(): Promise<{ content: object }> {
     const db = Container.get<mysql2.Connection>("db");
 
-    const queryUserkeyword = `
-    INSERT INTO CLElement (cl_element_id, problem, answer, public, cl_id)
-    VALUES ?
-    ON DUPLICATE KEY UPDATE problem = VALUES(problem), answer = VALUES(answer), modified_at = NOW()`;
+    const query = `SELECT * FROM BigField`;
+    const [dbResult] = await db.query(query);
+    const pResult = JSON.parse(JSON.stringify(dbResult));
 
-    let cl_id = 1;
-    let dummyCLES = [
-      {
-        cl_element_id: 1,
-        problem: "problem1",
-        answer: "answer1",
-        _public: 1,
-      },
-      {
-        cl_element_id: 2,
-        problem: "problem2",
-        answer: "answer2",
-        _public: 1,
-      },
-      {
-        cl_element_id: 3,
-        problem: "problem4",
-        answer: "answer4",
-        _public: 1,
-      },
-    ];
+    const query2 = `SELECT * FROM Field`;
+    const [dbResult2] = await db.query(query2);
+    const pResult2 = JSON.parse(JSON.stringify(dbResult2));
 
-    let rows: any = [];
-    dummyCLES.forEach((e) => {
-      let row = Object.values(e);
-      row.push(cl_id);
-      rows.push(row);
-    });
-    const [dbResult] = (await db.query(queryUserkeyword, [rows, rows])) as any; //as any라고 하면 사라짐.. ㅎㅎ ㅠㅠㅠ
-    console.log(dbResult.insertId);
-    console.log(dbResult.info); //"info": "Records: 3  Duplicates: 0  Warnings: 0",
+    let result = { bigField: [] } as any;
 
-    return { content: dbResult };
+    let idx = 1;
+    for (let i = 0; i < 14; i++) {
+      let json = {} as any;
+      json.id = pResult[i].big_field_id;
+      json.name = pResult[i].big_field;
+      json.smallGroup = [];
+
+      for (let j = 0; j < pResult2.length; j++) {
+        if (pResult2[j].big_field_id == idx) {
+          let sjson = {} as any;
+          sjson.id = pResult2[j].field_id;
+          sjson.name = pResult2[j].field;
+          json.smallGroup.push(sjson);
+        } else if (pResult2[j].big_field_id < idx) {
+          continue;
+        } else {
+          idx++;
+          break;
+        }
+      }
+
+      result.bigField.push(json);
+    }
+
+    console.log(result);
+
+    // fs.readFile(
+    //   path.join(__dirname, "../data.json"),
+    //   "utf8",
+    //   async (error, data) => {
+    //     if (error) throw error;
+    //     const pData = JSON.parse(data);
+    //   }
+    // );
+
+    return { content: result };
   }
-
-  //자기소개서항목 추가
-  //자기소개서항목 삭제
 }
